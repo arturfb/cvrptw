6cc662477e315507125848268fabf06a828da3e7
Marcelo Prates <marceloorp@gmail.com>
2016-11-22 12:21:26 -0200

    Added the relaxed version of TONN (RTONN)

--- src/VNS.hpp
+++ src/VNS.hpp
@@ -13,5 +13,6 @@ public:
 private:
    Instance* m_instance;
    Solution tonn(); // Time-oriented nearest-neighbor
+   Solution rtonn(Solution s); // Relaxed version of Time-oriented nearest-neighbor
    Solution twoOpt(Solution s, const unsigned k);
 };
